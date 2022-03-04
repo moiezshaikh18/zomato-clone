79c854bd7209a03088978fa278b12a0c091b0b7d
moiz <moiezshaikh18@gmail.com>
2022-03-04 23:44:38 +0530

    Working on header component

--- src/componets/common/header/index.js
+++ src/componets/common/header/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import "./header.css";
 const Header = () => {
   return (
-    <div className="max-width">
-      <img src="" alt=""></img>
+    <div className="max-width header">
+      <img
+        src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+        alt="Zomato-logo"
+        className="header-logo"
+      ></img>
+      <div className="header-left">
+        <div className="header-location-search-container"></div>
+        <div className="location-wrapper">
+          <div className="location-icon-name"></div>
+        </div>
+      </div>
     </div>
   );
 };
