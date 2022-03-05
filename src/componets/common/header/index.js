571ddaba8f823a189089ae4a1ffc5590665dd561
moiz <moiezshaikh18@gmail.com>
2022-03-05 23:23:47 +0530

    Search Feature done

--- src/componets/common/header/index.js
+++ src/componets/common/header/index.js
@@ -2,16 +2,29 @@ import React from "react";
 import "./header.css";
 const Header = () => {
   return (
-    <div className="max-width header">
+    <div className="header max-width">
       <img
         src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
         alt="Zomato-logo"
         className="header-logo"
-      ></img>
-      <div className="header-left">
-        <div className="header-location-search-container"></div>
-        <div className="location-wrapper">
-          <div className="location-icon-name"></div>
+      />
+      <div className="header-right">
+        <div className="header-location-search-container">
+          <div className="location-wrapper">
+            <div className="location-icon-name">
+              <i className="fa-solid fa-caret-down absolute-center location-icon"></i>
+              <div>Bangalore</div>
+            </div>
+            <i className="fa-solid fa-caret-down absolute-center"></i>
+          </div>
+          <div className="location-search-separator"></div>
+          <div className="header-searchBar">
+            <i className="fa-solid fa-magnifying-glass absolute-center search-icon"></i>
+            <input
+              className="search-input"
+              placeholder="Search for restaurant, cuisine or a dish"
+            />
+          </div>
         </div>
       </div>
     </div>
